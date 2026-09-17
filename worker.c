d5c3a996465116d25c61df29a53b81f871cb3183
ivannaranjo999 <ivanlink99@gmail.com>
2026-09-17 12:51:33 +0000

    fix: add to total_per_argument per thread using mutex

--- worker.c
+++ worker.c
@@ -10,14 +10,14 @@
 
 long *total_per_argument;
 
-void* worker_main(void *param){
-  (void)param;
+void* worker_main(){
+  long *local_totals = calloc(given_paths_size, sizeof(long));
 
   struct stack_entry entry;
   for (;;){
     int ret = pop_stack(&entry);
     if (ret == 0){
-      total_per_argument[entry.index] += get_size(entry);
+      local_totals[entry.index] += get_size(entry);
       free(entry.path);
       atomic_fetch_add(&files_done, 1);
       finish_work();
@@ -28,5 +28,12 @@ void* worker_main(void *param){
       usleep(WORKER_YIELD);
     }
   }
+
+  pthread_mutex_lock(&mutex);
+  for (int i = 0; i < given_paths_size; ++i){
+    total_per_argument[i] += local_totals[i];
+  }
+  pthread_mutex_unlock(&mutex);
+
   return NULL;
 }
